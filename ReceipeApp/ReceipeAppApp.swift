3bad134b5c0dfad5409e6b00c9f4f9ed9e5449ac
Isaac <isaacwellmax@outlook.com>
2024-05-21 13:15:17 -0500

    Initial Commit

---
+++ ReceipeApp/ReceipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceipeAppApp.swift
+//  ReceipeApp
+//
+//  Created by Isaac Maxwell Durairaj on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ReceipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
