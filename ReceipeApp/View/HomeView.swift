96809b07c0334f50d81d6c59851661e86349f8ed
Isaac <isaacwellmax@outlook.com>
2024-05-22 22:08:54 -0500

    Search added

--- ReceipeApp/View/HomeView.swift
+++ ReceipeApp/View/HomeView.swift
@@ -8,26 +8,48 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State var dessertList: [Dessert] = [Dessert]()
+    
+    @State private var dessertList: [Dessert] = [Dessert]()
+    @State private var searchText = ""
+
     var body: some View {
         NavigationView {
-            List(dessertList) { dessert in
-                VStack {
-                    NavigationLink {
-                        DessertMainView(mealId: dessert.idMeal)
-                            .navigationBarTitle(dessert.strMeal, displayMode: .inline)
-                    } label: {
-                        DessertListView(name: dessert.strMeal, url: dessert.strMealThumb, qty: nil)
-                    }
+            VStack {
+                ClippedTextField(text: $searchText)
+                    .padding()
+                List(dessertList.filter {
+                    self.searchText.isEmpty ||
+                    $0.strMeal.localizedCaseInsensitiveContains(searchText)
+                }) { dessert in
+//                    VStack {
+                        NavigationLink {
+                            DessertMainView(mealId: dessert.idMeal)
+                                .navigationBarTitle(dessert.strMeal, displayMode: .inline)
+                        } label: {
+                            DessertListView(name: dessert.strMeal, url: dessert.strMealThumb, qty: nil)
+                        }
+//                    }
                 }
                 .padding()
+
+                .task {
+                    dessertList = await ContentViewModel().displayDessertsClicked()!
+                }
+                .navigationTitle("Receipes")
             }
-            .task {
-                dessertList = await ContentViewModel().displayDessertsClicked()!
-            }
-            .navigationTitle("Receipes")
         }
         .listStyle(PlainListStyle())
+    }
+}
+
+struct ClippedTextField: View {
+    @Binding var text: String
+    
+    var body: some View {
+        TextField("Enter text", text: $text)
+            .padding()
+            .background(Color.gray.opacity(0.3))
+            .cornerRadius(10)
     }
 }
 
